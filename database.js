5c396a0588900cc49fda8b0db770ffb8fb3f171d
ASzzzzzzzz <alie.siaz@gmail.com>
2025-06-11 23:50:36 +0700

    Update database.js

--- database.js
+++ database.js
@@ -4,16 +4,16 @@
 
 const productDatabase = {
     'monogram-tee': {
-        name: 'Runz Black',
-        price: 'Rp 230.000',
+        name: 'Monogram Tee',
+        price: 'Rp 450.000',
         description: 'Kaos katun premium dengan cetakan monogram SIAZ* yang subtle di bagian dada. Potongan regular fit yang nyaman untuk dipakai sehari-hari, memberikan kesan minimalis namun tetap berkarakter.',
         images: [
-            './images/RUNZ__2b.jpg/text=SIAZ*+Runz+Model',
+            '.images/RUNZ__2b.jpg',
             'https://placehold.co/800x1000/e0e0e0/333333?text=SIAZ*+Tee+Belakang',
             'https://placehold.co/800x1000/dcdcdc/333333?text=Detail+Kerah',
             'https://placehold.co/800x1000/d6d6d6/333333?text=Detail+Bahan',
         ],
-        buyLink: 'https://instagram.com/siaz_official',
+        buyLink: 'https://instagram.com/siaz',
         relatedItems: ['tech-cargo', 'nylon-cap']
     },
     'utility-hoodie': {
